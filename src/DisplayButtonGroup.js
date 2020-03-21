49020654016c5fab2de1d4dead45aeacab613f2b
sandeepgokhale <52096984+sandeepgokhale@users.noreply.github.com>
2020-03-21 20:35:10 +0530

    Added More Questions
    And Reordered Buttons

--- src/DisplayButtonGroup.js
+++ src/DisplayButtonGroup.js
@@ -47,6 +47,19 @@ const DisplayButtonGroup = ({
       >
         Submit Answer
       </button>
+
+      <button
+        disabled={questionNo === questions.length - 1 ? true : false}
+        className={
+          questionNo !== questions.length - 1 ? "btn btnBlue" : "btn btnGrey"
+        }
+        onClick={() => {
+          setQuestionNo(questionNo + 1);
+          setTempRadioSelection("");
+        }}
+      >
+        Next
+      </button>
       <button
         className={
           questions[questionNo].answeredRequested === true
@@ -59,18 +72,6 @@ const DisplayButtonGroup = ({
         onClick={() => revealAnswer()}
       >
         Show Answer
-      </button>
-      <button
-        disabled={questionNo === questions.length - 1 ? true : false}
-        className={
-          questionNo !== questions.length - 1 ? "btn btnBlue" : "btn btnGrey"
-        }
-        onClick={() => {
-          setQuestionNo(questionNo + 1);
-          setTempRadioSelection("");
-        }}
-      >
-        Next
       </button>
     </>
   );
--- src/constants.js
+++ src/constants.js
@@ -12,7 +12,7 @@ export const question = [
   {
     questionNo: "1",
     question: "2. Who won the ICC ODI World Cup in 1996",
-    options: [["2India", "M"], ["Australia", "F"], ["Other", "O"]],
+    options: [["India", "M"], ["Australia", "F"], ["Other", "O"]],
     answer: "F",
     attemptedAnswer: "",
     answeredCorrectly: false,
@@ -21,7 +21,7 @@ export const question = [
   {
     questionNo: "2",
     question: "3. Who won the ICC ODI World Cup in 2017",
-    options: [["3India", "M"], ["Australia", "F"], ["Other", "O"]],
+    options: [["India", "M"], ["Australia", "F"], ["Other", "O"]],
     answer: "O",
     attemptedAnswer: "",
     answeredSubmitted: false,
@@ -32,6 +32,37 @@ export const question = [
     question: "4. Who Invented Bulb",
     options: [["Edison", "E"], ["Sandeep Gokhale", "S"], ["Other", "O"]],
     answer: "E",
+    attemptedAnswer: "",
+    answeredSubmitted: false,
+    answeredRequested: false
+  },
+  {
+    questionNo: "4",
+    question: "5. Who Invented Zero",
+    options: [
+      ["Brahmagupta", "B"],
+      ["Sandeep Gokhale", "S"],
+      ["Aryabhatta", "O"]
+    ],
+    answer: "B",
+    attemptedAnswer: "",
+    answeredSubmitted: false,
+    answeredRequested: false
+  },
+  {
+    questionNo: "5",
+    question: "6. How many hours do we have in a day?",
+    options: [["24", "24"], ["25", "25"], ["26", "26"]],
+    answer: "24",
+    attemptedAnswer: "",
+    answeredSubmitted: false,
+    answeredRequested: false
+  },
+  {
+    questionNo: "6",
+    question: "7. How many days do we have in a week?",
+    options: [["7", "7"], ["8", "8"], ["9", "9"]],
+    answer: "7",
     attemptedAnswer: "",
     answeredSubmitted: false,
     answeredRequested: false
